bc14f02bcfca439a399d5512acf8b34901f137e2
Ryan Guo <guohao_lin@msn.com>
2018-12-16 00:23:19 -0800

    Update Smileys.java

--- Exercise 1 Smileys/Smileys.java
+++ Exercise 1 Smileys/Smileys.java
@@ -23,3 +23,5 @@ public class Smileys {
      public static void main(String[] args) {
         // Test your method at least with the following
          printWithSmileys("Method");
+
+         printWithSmileys("Beerbottle");
