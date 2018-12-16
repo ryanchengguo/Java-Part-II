e1f3b26d61d29370c8612e72070289abf0040f7a
Ryan Guo <guohao_lin@msn.com>
2018-12-16 00:22:33 -0800

    Update Smileys.java

--- Exercise 1 Smileys/Smileys.java
+++ Exercise 1 Smileys/Smileys.java
@@ -1,4 +1,13 @@
-
 public class Smileys {
-
-}
+    private static void printWithSmileys(String characterString) {
+        int stringLength = characterString.length();
+        int nSmileys = (stringLength + 1) / 2 + 3;
+        
+        printSmiley(nSmileys);
+        if (stringLength % 2 == 0) {
+            System.out.println(":) " + characterString + " :)");
+        } else {
+            System.out.println(":) " + characterString + "  :)");
+        }
+        printSmiley(nSmileys);
+    }
