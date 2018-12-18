d9675ad6e33a2fc4f102e0058b34a4e354786b67
Ryan Guo <guohao_lin@msn.com>
2018-12-17 23:23:56 -0800

    Update Calculator.java

--- Exercise 03 Calculator/Calculator.java
+++ Exercise 03 Calculator/Calculator.java
@@ -1,5 +1,8 @@
-package exercise;
-
 public class Calculator {
-
-}
+    private Reader reader;
+    private int numOfCalculations;
+    
+    public Calculator() {
+        reader = new Reader();
+        numOfCalculations = 0;
+    }
