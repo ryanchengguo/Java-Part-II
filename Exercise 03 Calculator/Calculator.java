f7c3663ff356b6e2a95ad76af038fa7e136d0d54
Ryan Guo <guohao_lin@msn.com>
2018-12-17 23:24:07 -0800

    Update Calculator.java

--- Exercise 03 Calculator/Calculator.java
+++ Exercise 03 Calculator/Calculator.java
@@ -6,3 +6,14 @@ public class Calculator {
         reader = new Reader();
         numOfCalculations = 0;
     }
+
+    private int[] getValues() {
+        int[] values = new int[2];
+        
+        System.out.print("value1: ");
+        values[0] = reader.readInteger();
+        System.out.print("value2: ");
+        values[1] = reader.readInteger();
+        
+        return values;
+    }
