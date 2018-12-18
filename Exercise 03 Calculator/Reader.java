4e00d06243f1a648a9ae57a6548a82fe6e51bcea
Ryan Guo <guohao_lin@msn.com>
2018-12-17 23:25:13 -0800

    Update Reader.java

--- Exercise 03 Calculator/Reader.java
+++ Exercise 03 Calculator/Reader.java
@@ -1,5 +1,8 @@
-package exercise;
+import java.util.Scanner;
 
 public class Reader {
-
-}
+    private Scanner scanner;
+    
+    public Reader() {
+        scanner = new Scanner(System.in);
+    }
