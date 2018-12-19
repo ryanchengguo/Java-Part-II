f7a1fd184855516826804f9958ac4db7da6178e2
Ryan Guo <guohao_lin@msn.com>
2018-12-18 23:44:57 -0800

    Update Suitcase.java

--- Exercise 4 Thing Suitcase and Container/Suitcase.java
+++ Exercise 4 Thing Suitcase and Container/Suitcase.java
@@ -8,3 +8,17 @@ public class Suitcase {
         this.things = new ArrayList<Thing>();
         this.maxWeight = maxWeight;
     }
+
+    public void addThing(Thing thing) {
+        if (totalWeight() + thing.getWeight() <= maxWeight) {
+            things.add(thing);
+        }
+    }
+    
+    public void printThings() {
+        System.out.println("Your suitcase contains the following things:");
+        
+        for (Thing thing : things) {
+            System.out.println(thing);  // Automatically calls "toString()"
+        }
+    }
