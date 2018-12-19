9e5de0d6b82a167cbc4f3dbf42e49aa5b8987bb2
Ryan Guo <guohao_lin@msn.com>
2018-12-18 23:43:42 -0800

    Update Container.java

--- Exercise 4 Thing Suitcase and Container/Container.java
+++ Exercise 4 Thing Suitcase and Container/Container.java
@@ -1,4 +1,11 @@
+import java.util.ArrayList;
 
 public class Container {
-
-}
+    private ArrayList<Suitcase> suitcases;
+    private int maxWeight;
+    
+    public Container(int maxWeight) {
+        this.suitcases = new ArrayList<Suitcase>();
+        this.maxWeight = maxWeight;
+    }
+    
