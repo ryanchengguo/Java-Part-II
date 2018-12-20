46409b093477e071e5c30d091b11c315e121b4eb
Ryan Guo <guohao_lin@msn.com>
2018-12-19 23:41:35 -0800

    Update Thing.java

--- Exercise 4 Thing Suitcase and Container/Thing.java
+++ Exercise 4 Thing Suitcase and Container/Thing.java
@@ -1,3 +1,8 @@
 public class Thing {
     private String name;
     private int weight;
+
+    public Thing(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
