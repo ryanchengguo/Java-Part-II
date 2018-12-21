24eb2aa638953cebe37d50ff8d6e0c2b0375eed3
Ryan Guo <guohao_lin@msn.com>
2018-12-20 23:50:33 -0800

    Update PromissoryNote.java

--- Exercise 6 Promissory Note/PromissoryNote.java
+++ Exercise 6 Promissory Note/PromissoryNote.java
@@ -2,3 +2,7 @@ import java.util.HashMap;
 
 public class PromissoryNote {
       private HashMap<String, Double> loans;
+
+      public PromissoryNote() {
+        loans = new HashMap<String, Double>();
+    }
