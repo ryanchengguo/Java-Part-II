8b635868982f10a31e63db39991c5412ced3962d
Ryan Guo <guohao_lin@msn.com>
2018-12-23 21:39:56 -0800

    Update Airport.java

--- Exercise 8 Airport/Airport.java
+++ Exercise 8 Airport/Airport.java
@@ -1,4 +1,11 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class Smileys {
-
-}
+public class Airport {
+    private ArrayList<Plane> planes;
+    private HashMap<Plane, Flight> flights;
+    
+    public Airport() {
+        planes = new ArrayList<Plane>();
+        flights = new HashMap<Plane, Flight>();
+    }
