6593f6200959ccee3b708654da8cd6b30672be48
Ryan Guo <guohao_lin@msn.com>
2018-12-23 21:41:08 -0800

    Update Flight.java

--- Exercise 8 Airport/Flight.java
+++ Exercise 8 Airport/Flight.java
@@ -1,3 +1,8 @@
 public class Flight {
     private String departureCode;
     private String destinationCode;
+
+    public Flight(String departureCode, String destinationCode) {
+        this.departureCode = departureCode;
+        this.destinationCode = destinationCode;
+    }
