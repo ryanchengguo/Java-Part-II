09dd406a08ec55c971d46c6d1f79a8e10086d8b9
Ryan Guo <guohao_lin@msn.com>
2018-12-23 21:40:07 -0800

    Update Airport.java

--- Exercise 8 Airport/Airport.java
+++ Exercise 8 Airport/Airport.java
@@ -9,3 +9,17 @@ public class Airport {
         planes = new ArrayList<Plane>();
         flights = new HashMap<Plane, Flight>();
     }
+
+    public void addPlane(String planeID, int capacity) {
+        planes.add(new Plane(planeID, capacity));
+    }
+    
+    public void addFlight(String planeID, Flight flight) {
+        for (Plane p : planes) {
+            if (p.getPlaneID().equals(planeID)) {
+                Plane plane = new Plane(p.getPlaneID(), p.getCapacity());
+                flights.put(plane, flight);
+                break;
+            }
+        }
+    }
