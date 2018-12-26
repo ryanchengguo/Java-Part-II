66b578ca67193c49a0991a4eddd5881aae8de0a2
Ryan Guo <guohao_lin@msn.com>
2018-12-26 02:10:53 -0800

    Update Main.java

--- Exercise 8 Airport/Main.java
+++ Exercise 8 Airport/Main.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
 
-public class Smileys {
-
-}
+public class Main {
+    public static void main(String[] args) {
+        String input = "1\n"  + "HA-LOL\n" + "42\n" +
+                   "1\n"  + "G-OWAC\n" + "101\n" +
+                   "2\n"  + "HA-LOL\n" + "HEL\n" +
+                   "BAL\n"  + "2\n" + "G-OWAC\n" +
+                   "JFK\n"  + "BAL\n" + "2\n" +
+                   "HA-LOL\n" + "BAL\n" + "HEL\n" +
+                    "x\n" + "1\n" + "2\n" + "3\n" +
+                    "G-OWAC\n" + "x";
