668c882b1fa3654ed5919a378c367d8fd06c7bc8
Ryan Guo <guohao_lin@msn.com>
2018-12-26 02:11:34 -0800

    Update Plane.java

--- Exercise 8 Airport/Plane.java
+++ Exercise 8 Airport/Plane.java
@@ -1,3 +1,12 @@
 public class Plane {
     private String planeID;
     private int capacity;
+
+    public Plane(String planeID, int capacity) {
+        this.planeID = planeID;
+        this.capacity = capacity;
+    }
+    
+    public String getPlaneID() {
+        return planeID;
+    }
