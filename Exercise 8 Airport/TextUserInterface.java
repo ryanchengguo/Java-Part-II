92aa207e9c788b2a5482cc239344e8ed10e1504d
Ryan Guo <guohao_lin@msn.com>
2018-12-28 04:08:27 -0800

    Update TextUserInterface.java

--- Exercise 8 Airport/TextUserInterface.java
+++ Exercise 8 Airport/TextUserInterface.java
@@ -3,3 +3,8 @@ import java.util.Scanner;
 public class TextUserInterface {
     private Scanner reader;
     private Airport airport;
+
+    public TextUserInterface(Scanner reader, Airport airport) {
+        this.reader = reader;
+        this.airport = airport;
+    }
