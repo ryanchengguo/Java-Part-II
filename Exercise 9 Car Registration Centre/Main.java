65b349473d311ff83129590d7572bbf6f123e30a
Ryan Guo <guohao_lin@msn.com>
2018-12-31 01:47:14 -0800

    Update Main.java

--- Exercise 9 Car Registration Centre/Main.java
+++ Exercise 9 Car Registration Centre/Main.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class Smileys {
+public class Main {
 
-}
+    public static void main(String[] args) {
+
+        RegistrationPlate reg1 = new RegistrationPlate("FI", "ABC-123");
+        RegistrationPlate reg2 = new RegistrationPlate("FI", "UXE-465");
+        RegistrationPlate reg3 = new RegistrationPlate("D", "B WQ-431");
+
+        List<RegistrationPlate> finnish = new ArrayList<RegistrationPlate>();
+        finnish.add(reg1);
+        finnish.add(reg2);
+
+        RegistrationPlate newPlate = new RegistrationPlate("FI", "ABC-123");
+
+        if (!finnish.contains(newPlate)) {
+            finnish.add(newPlate);
+        }
