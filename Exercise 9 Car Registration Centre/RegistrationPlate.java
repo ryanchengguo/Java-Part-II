8921385a067e704305d4ee67d13f8ae93a96f225
Ryan Guo <guohao_lin@msn.com>
2018-12-31 01:47:56 -0800

    Update RegistrationPlate.java

--- Exercise 9 Car Registration Centre/RegistrationPlate.java
+++ Exercise 9 Car Registration Centre/RegistrationPlate.java
@@ -1,4 +1,11 @@
+public class RegistrationPlate {
+    // don't change the code which is already given to you
 
-public class Smileys {
+    // ATTENTION: the object variable types are final, meaning that their value cannot be changed!
+    private final String regCode;
+    private final String country;
 
-}
+    public RegistrationPlate(String country, String regCode) {
+        this.regCode = regCode;
+        this.country = country;
+    }
