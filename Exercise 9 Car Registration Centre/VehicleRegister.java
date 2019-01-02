3a6d58637d4af638cb7981c981eb44c7afee571b
Ryan Guo <guohao_lin@msn.com>
2019-01-02 05:43:30 -0800

    Update VehicleRegister.java

--- Exercise 9 Car Registration Centre/VehicleRegister.java
+++ Exercise 9 Car Registration Centre/VehicleRegister.java
@@ -2,3 +2,9 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
+public class VehicleRegister {
+    private HashMap<RegistrationPlate, String> owners;
+    
+    public VehicleRegister() {
+        owners = new HashMap<RegistrationPlate, String>();
+    }
