9082e97efcd3a8d7faf7dbb292c4ff1f0b5087af
Ryan Guo <guohao_lin@msn.com>
2019-01-03 06:59:40 -0800

    Update NatinalService.java

--- Exercise 10 National Service/NatinalService.java
+++ Exercise 10 National Service/NatinalService.java
@@ -1,19 +1,4 @@
-public class CivilService implements NationalService {
-    private int daysLeft;
-
-    public CivilService() {
-        daysLeft = 362;
-    }
-
-    @Override
-    public int getDaysLeft() {
-        return daysLeft;
-    }
-
-    @Override
-    public void work() {
-        if (daysLeft > 0) {
-            daysLeft--;
-        }
-    }
+public interface NationalService {
+    int getDaysLeft();
+    void work();
 }
