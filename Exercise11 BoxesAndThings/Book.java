6e8711e5492f696399721eab16d1e196ca14aea8
Ryan Guo <guohao_lin@msn.com>
2019-01-07 01:38:18 -0800

    Update Book.java

--- Exercise11 BoxesAndThings/Book.java
+++ Exercise11 BoxesAndThings/Book.java
@@ -1,4 +1,20 @@
-
-public class Smileys {
-
+public class Book implements ToBeStored {
+    private String writer;
+    private String name;
+    private double weight;
+    
+    public Book(String writer, String name, double weight) {
+        this.writer = writer;
+        this.name = name;
+        this.weight = weight;
+    }
+    
+    public double weight() {
+        return weight;
+    }
+    
+    @Override
+    public String toString() {
+        return writer + ": " + name;
+    }
 }
