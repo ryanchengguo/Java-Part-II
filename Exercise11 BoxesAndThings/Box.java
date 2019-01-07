bd9efd2c6c8012be8aef9dce615fd5a846e202f7
Ryan Guo <guohao_lin@msn.com>
2019-01-07 01:38:37 -0800

    Update Box.java

--- Exercise11 BoxesAndThings/Box.java
+++ Exercise11 BoxesAndThings/Box.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
 
-public class Smileys {
-
-}
+public class Box implements ToBeStored {
+    private ArrayList<ToBeStored> things;
+    private double maxWeight;
+    
+    public Box(double maxWeight) {
+        this.things = new ArrayList<ToBeStored>();
+        this.maxWeight = maxWeight;
+    }
