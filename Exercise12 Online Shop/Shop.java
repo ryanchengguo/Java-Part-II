6cbcae655c7c427c94b3f662968f36d6b9dfab8c
Ryan Guo <guohao_lin@msn.com>
2019-01-09 18:58:23 -0800

    Update Shop.java

--- Exercise12 Online Shop/Shop.java
+++ Exercise12 Online Shop/Shop.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
 
-public class Smileys {
+public class Shop {
+    private Storehouse store;
+    private Scanner reader;
 
-}
+    public Shop(Storehouse store, Scanner reader) {
+        this.store = store;
+        this.reader = reader;
+    }
