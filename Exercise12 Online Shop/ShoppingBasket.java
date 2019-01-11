7aea592231889c65b0a94f9790d3266eac5b0a92
Ryan Guo <guohao_lin@msn.com>
2019-01-10 21:31:31 -0800

    Update ShoppingBasket.java

--- Exercise12 Online Shop/ShoppingBasket.java
+++ Exercise12 Online Shop/ShoppingBasket.java
@@ -1,4 +1,9 @@
+import java.util.HashMap;
+import java.util.Map;
 
-public class Smileys {
-
-}
+public class ShoppingBasket {
+    private Map<String, Purchase> purchases;
+    
+    public ShoppingBasket() {
+        purchases = new HashMap<String, Purchase>();
+    }
