cdb4701937bca2718e5a3aa19ecdbd91e310c10b
Ryan Guo <guohao_lin@msn.com>
2019-01-10 21:32:25 -0800

    Update Storehouse.java

--- Exercise12 Online Shop/Storehouse.java
+++ Exercise12 Online Shop/Storehouse.java
@@ -1,4 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
-public class Smileys {
-
-}
+public class Storehouse {
+    private Map<String, Integer> productPrices;
+    private Map<String, Integer> productStock;
+    
+    public Storehouse() {
+        productPrices = new HashMap<String, Integer>();
+        productStock = new HashMap<String, Integer>();
+    }
