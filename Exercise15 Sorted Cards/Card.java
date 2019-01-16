64f5fa8f80c1d6585f043d047f5bff20f6d04df2
Ryan Guo <guohao_lin@msn.com>
2019-01-15 19:17:44 -0800

    Update Card.java

--- Exercise15 Sorted Cards/Card.java
+++ Exercise15 Sorted Cards/Card.java
@@ -8,3 +8,13 @@ public class Card implements Comparable<Card> {
     public static final int DIAMONDS = 1;
     public static final int HEARTS = 2;
     public static final int CLUBS = 3;
+
+    /*
+     * To make printing easier, Card-class also has string arrays for suits and values.
+     * SUITS[suit] is a string representation of the suit (Clubs, Diamonds, Hearts, Spades)
+     * VALUES[value] is an abbreviation of the card's value (A, J, Q, K, [2..10]).
+     */
+    public static final String[] SUITS = {"Spades", "Diamonds", "Hearts", "Clubs"};
+    public static final String[] VALUES = {"-", "-", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    private int value;
+    private int suit;
