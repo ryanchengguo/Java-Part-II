1628c04e10e9850922cd5ce3b59dc77c5391495c
Ryan Guo <guohao_lin@msn.com>
2019-01-15 19:17:34 -0800

    Update Card.java

--- Exercise15 Sorted Cards/Card.java
+++ Exercise15 Sorted Cards/Card.java
@@ -1,4 +1,10 @@
+public class Card implements Comparable<Card> {
 
-public class Smileys {
-
-}
+    /*
+     * These are static constant variables. These variables can be used inside and outside
+     * of this class like, for example, Card.CLUBS
+     */
+    public static final int SPADES = 0;
+    public static final int DIAMONDS = 1;
+    public static final int HEARTS = 2;
+    public static final int CLUBS = 3;
