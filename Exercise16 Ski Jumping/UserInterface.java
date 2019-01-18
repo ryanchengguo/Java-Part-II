89f57275692193a06788970df83a65d094f50494
Ryan Guo <guohao_lin@msn.com>
2019-01-18 13:44:26 -0800

    Update UserInterface.java

--- Exercise16 Ski Jumping/UserInterface.java
+++ Exercise16 Ski Jumping/UserInterface.java
@@ -1,7 +1,3 @@
-
-public class HelloWorld {
-	public static void main(String [ ] args){
-		System.out.println("Hello world!");
-	}
-	
-}
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
