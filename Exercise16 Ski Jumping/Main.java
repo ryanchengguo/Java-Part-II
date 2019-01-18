3b60b5e26732676cb0133129c1a00b4df445d44b
Ryan Guo <guohao_lin@msn.com>
2019-01-18 13:40:45 -0800

    Update Main.java

--- Exercise16 Ski Jumping/Main.java
+++ Exercise16 Ski Jumping/Main.java
@@ -1,7 +1,10 @@
 
-public class HelloWorld {
-	public static void main(String [ ] args){
-		System.out.println("Hello world!");
-	}
-	
+public class Main {
+
+    public static void main(String[] args) {
+
+        UserInterface ui = new UserInterface();
+
+        ui.start();
+    }
 }
