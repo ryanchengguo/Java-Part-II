c93e0a0ce1fc18449c60abfd2e714fab86128293
Ryan Guo <guohao_lin@msn.com>
2019-01-18 13:41:07 -0800

    Update Participant.java

--- Exercise16 Ski Jumping/Participant.java
+++ Exercise16 Ski Jumping/Participant.java
@@ -1,7 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class HelloWorld {
-	public static void main(String [ ] args){
-		System.out.println("Hello world!");
-	}
-	
-}
+public class Participant implements Comparable<Participant> {
+    
+    private String name;
+    private Map<Integer, Points> points;
