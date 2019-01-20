c95d27b5e007db0b8d5fed373e3c626a37dfac31
Ryan Guo <guohao_lin@msn.com>
2019-01-20 00:05:01 -0800

    Update Tournament.java

--- Exercise16 Ski Jumping/Tournament.java
+++ Exercise16 Ski Jumping/Tournament.java
@@ -1,3 +1,9 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+public class Tournament {
+
+    private List<Participant> participants;
+    private Points points;
+    private int roundNum;
