b30fb2bc170649b33f73ac7027189e54f01ffa48
Ryan Guo <guohao_lin@msn.com>
2019-01-20 00:05:10 -0800

    Update Tournament.java

--- Exercise16 Ski Jumping/Tournament.java
+++ Exercise16 Ski Jumping/Tournament.java
@@ -7,3 +7,13 @@ public class Tournament {
     private List<Participant> participants;
     private Points points;
     private int roundNum;
+
+    public Tournament() {
+        this.participants = new ArrayList<Participant>();
+        this.points = new Points();
+        this.roundNum = 1;
+    }
+    
+    public void addParticipant(String name) {
+        participants.add(new Participant(name));
+    }
