2114f624f2a564649eac7beb6cb5de940bb5f555
Ryan Guo <guohao_lin@msn.com>
2019-01-21 22:00:34 -0800

    Update UserInterface.java

--- Exercise16 Ski Jumping/UserInterface.java
+++ Exercise16 Ski Jumping/UserInterface.java
@@ -6,3 +6,8 @@ public class UserInterface {
 
     private Scanner reader;
     private Tournament tournament;
+
+    public UserInterface() {
+        reader = new Scanner(System.in);
+        tournament = new Tournament();
+    }
