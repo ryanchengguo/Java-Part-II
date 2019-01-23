80f07057b92b5eccc3b7bfdb15ca7d0e3052fc05
Ryan Guo <guohao_lin@msn.com>
2019-01-22 23:37:59 -0800

    Update ApplicationLogic.java

--- Exercise17 First Packages/src/mooc/logic/ApplicationLogic.java
+++ Exercise17 First Packages/src/mooc/logic/ApplicationLogic.java
@@ -9,3 +9,11 @@ public class ApplicationLogic {
     public ApplicationLogic(UserInterface ui) {
         this.ui = ui;
     }
+
+    public void execute(int howManyTimes) {
+        for (int i = 0; i < howManyTimes; i++) {
+            System.out.println("The application logic works");
+            ui.update();
+        }
+    }
+}
