75630183cc1c96a31928d29bb9e4b526701dfa20
Ryan Guo <guohao_lin@msn.com>
2019-01-22 23:37:49 -0800

    Update ApplicationLogic.java

--- Exercise17 First Packages/src/mooc/logic/ApplicationLogic.java
+++ Exercise17 First Packages/src/mooc/logic/ApplicationLogic.java
@@ -1,3 +1,11 @@
 package mooc.logic;
 
 import mooc.ui.UserInterface;
+
+public class ApplicationLogic {
+    
+    private UserInterface ui;
+    
+    public ApplicationLogic(UserInterface ui) {
+        this.ui = ui;
+    }
