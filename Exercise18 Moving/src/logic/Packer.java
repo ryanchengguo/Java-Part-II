2ea8da406e586022dd1bbf23e88d7f51409ccad1
Ryan Guo <guohao_lin@msn.com>
2019-01-23 23:32:29 -0800

    Update Packer.java

--- Exercise18 Moving/src/logic/Packer.java
+++ Exercise18 Moving/src/logic/Packer.java
@@ -4,3 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import moving.domain.Box;
 import moving.domain.Thing;
+
+public class Packer {
+
+    private int boxesVolume;
+
+    public Packer(int boxesVolume) {
+        this.boxesVolume = boxesVolume;
+    }
+  
+}
