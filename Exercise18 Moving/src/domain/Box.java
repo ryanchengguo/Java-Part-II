1044b79b027dc106beee5e0885a1443760e2f2b2
Ryan Guo <guohao_lin@msn.com>
2019-01-23 23:30:58 -0800

    Update Box.java

--- Exercise18 Moving/src/domain/Box.java
+++ Exercise18 Moving/src/domain/Box.java
@@ -2,3 +2,13 @@ package moving.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+
+public class Box implements Thing {
+
+    private int maximumCapacity;
+    private List<Thing> things;
+
+    public Box(int maximumCapacity) {
+        this.maximumCapacity = maximumCapacity;
+        this.things = new ArrayList<Thing>();
+    }
