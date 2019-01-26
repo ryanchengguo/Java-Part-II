478f80bfe013cd6bc2606274135660244c2a6b58
Ryan Guo <guohao_lin@msn.com>
2019-01-25 22:14:39 -0800

    Update Calculator.java

--- Exercise19 Method Argument Validation/Calculator.java
+++ Exercise19 Method Argument Validation/Calculator.java
@@ -6,3 +6,11 @@ public class Calculator {
         if (fromInteger < 0) {
             throw new IllegalArgumentException();
         }
+
+        int multiplication = 1;
+        for (int i = 1; i <= fromInteger; i++) {
+            multiplication *= i;
+        }
+
+        return multiplication;
+    }
