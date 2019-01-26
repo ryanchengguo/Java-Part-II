59de382a0d72f91fb7a99a5cb00f09795890ba6b
Ryan Guo <guohao_lin@msn.com>
2019-01-25 22:15:26 -0800

    Update Person.java

--- Exercise19 Method Argument Validation/Person.java
+++ Exercise19 Method Argument Validation/Person.java
@@ -4,3 +4,12 @@ public class Person {
 
     private String name;
     private int age;
+
+    public Person(String name, int age) {
+        if (name == null || name.isEmpty() || name.length() > 40 
+                || age < 0 || age > 120) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+        this.age = age;
+    }
