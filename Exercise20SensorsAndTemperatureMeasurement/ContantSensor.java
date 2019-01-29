4e59b108df61485549de035c27a1df3be7ec8234
Ryan Guo <guohao_lin@msn.com>
2019-01-28 23:36:31 -0800

    Update ContantSensor.java

--- Exercise20SensorsAndTemperatureMeasurement/ContantSensor.java
+++ Exercise20SensorsAndTemperatureMeasurement/ContantSensor.java
@@ -2,4 +2,9 @@ package application;
 
 public class ConstantSensor implements Sensor {
   
+  private int reading;
+
+    public ConstantSensor(int reading) {
+        this.reading = reading;
+    }
 }
