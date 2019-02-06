0a292c2a8ec7aad4f70f8878710f39ea37335c48
Ryan Guo <guohao_lin@msn.com>
2019-02-06 10:48:11 -0800

    Update Main.java

--- Exercise21 Printer/Main.java
+++ Exercise21 Printer/Main.java
@@ -1,25 +1,12 @@
 public class Main {
 
-
-
     public static void main(String[] args) throws Exception {
-
         Printer printer = new Printer("src/textfile.txt");
-
-
-
         printer.printLinesWhichContain("Väinämöinen");
-
         System.out.println("-----");
-
         printer.printLinesWhichContain("Frank Zappa");
-
         System.out.println("-----");
-
         printer.printLinesWhichContain("");
-
         System.out.println("-----");
-
     }
-
 }
