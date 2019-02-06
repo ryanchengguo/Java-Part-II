15cabad6626860261e50905b11fe160467cc4ba9
Ryan Guo <guohao_lin@msn.com>
2019-02-06 10:47:49 -0800

    Update Main.java

--- Exercise21 Printer/Main.java
+++ Exercise21 Printer/Main.java
@@ -1,27 +1,25 @@
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Queue;
+public class Main {
 
-/*
- * SD2x Homework #2
- * This class contains a method that will read an HTML file and convert it to a Queue of HtmlTags.
- * It is simply provided as a convenience class for you to use during your testing.
- * You do not need to submit this code.
- */
 
-public class HtmlReader {
-	
-	public static Queue<HtmlTag> getTagsFromHtmlFile(String filename) throws IOException {
-	     InputStream stream = new FileInputStream(filename);
-	     StringBuffer buffer = new StringBuffer();
-	     int ch;
-	     while ((ch = stream.read()) > 0) {
-	         buffer.append((char) ch);
-	     }
-	     stream.close();
-	     String htmlFileString = buffer.toString();
-	     return HtmlTag.tokenize(htmlFileString);
-	}
+
+    public static void main(String[] args) throws Exception {
+
+        Printer printer = new Printer("src/textfile.txt");
+
+
+
+        printer.printLinesWhichContain("Väinämöinen");
+
+        System.out.println("-----");
+
+        printer.printLinesWhichContain("Frank Zappa");
+
+        System.out.println("-----");
+
+        printer.printLinesWhichContain("");
+
+        System.out.println("-----");
+
+    }
 
 }
