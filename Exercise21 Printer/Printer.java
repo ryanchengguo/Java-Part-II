e436d6d6eaaedd53ce24f85ce9abac5fb57fc44b
Ryan Guo <guohao_lin@msn.com>
2019-02-06 10:49:45 -0800

    Update Printer.java

--- Exercise21 Printer/Printer.java
+++ Exercise21 Printer/Printer.java
@@ -1,2 +1,11 @@
 import java.io.*;
 import java.util.*;
+
+public class Printer {
+
+    private Scanner reader;
+    private File f;
+
+    public Printer(String filename) throws Exception {
+        this.f = new File(filename);
+    }
