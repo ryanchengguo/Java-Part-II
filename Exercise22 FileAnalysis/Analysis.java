cbb9f92b9e4eab5d3348169a780ee4e17779436f
Ryan Guo <guohao_lin@msn.com>
2019-02-11 23:39:49 -0800

    Update Analysis.java

--- Exercise22 FileAnalysis/Analysis.java
+++ Exercise22 FileAnalysis/Analysis.java
@@ -3,3 +3,11 @@ package file;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
+public class Analysis {
+    
+    private File file;
+    
+    public Analysis(File file) {
+        this.file = file;
+    }
