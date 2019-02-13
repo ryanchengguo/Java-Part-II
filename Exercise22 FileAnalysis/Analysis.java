3d58dc56a10c2cb749a15b07e89c335f264942fc
Ryan Guo <guohao_lin@msn.com>
2019-02-12 23:53:49 -0800

    Update Analysis.java

--- Exercise22 FileAnalysis/Analysis.java
+++ Exercise22 FileAnalysis/Analysis.java
@@ -25,3 +25,15 @@ public class Analysis {
     }
 
    
+ public int characters() throws Exception {
+        Scanner reader = new Scanner(file, "UTF-8");
+        int nChars = 0;
+
+        while (reader.hasNext()) {
+            String line = reader.nextLine();
+            nChars += line.length() + 1;  // +1 for line breaks
+        }
+
+        return nChars;
+    }
+}
