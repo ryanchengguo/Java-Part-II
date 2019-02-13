4f1176d9bf41e17fd050e307e091ee30689b4cd4
Ryan Guo <guohao_lin@msn.com>
2019-02-12 23:53:18 -0800

    Update Analysis.java

--- Exercise22 FileAnalysis/Analysis.java
+++ Exercise22 FileAnalysis/Analysis.java
@@ -6,23 +6,7 @@ import java.util.Scanner;
 
 public class Analysis {
     
-    private File file;
     
-    public Analysis(File file) {
-        this.file = file;
-    }
-
-    public int lines() throws Exception {
-        Scanner reader = new Scanner(file, "UTF-8");
-        int nLines = 0;
-        
-        while (reader.hasNext()) {
-            nLines++;
-            reader.nextLine();
-        }
-        
-        return nLines;
-    }
 
     public int characters() throws Exception {
         Scanner reader = new Scanner(file, "UTF-8");
