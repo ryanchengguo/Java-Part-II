cf7b9d7f73a5c18cc86d92e1070972351484c7ba
Ryan Guo <guohao_lin@msn.com>
2019-02-12 23:54:19 -0800

    Update Main.java

--- Exercise22 FileAnalysis/Main.java
+++ Exercise22 FileAnalysis/Main.java
@@ -1,4 +1,12 @@
+package file;
 
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+            
         File file = new File("src/testfile.txt");
+            
     }
 }
